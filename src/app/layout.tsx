1fc897633c89f91361146f45e14f39e69e0a4628
Lucas Webber <lucas@lucaswebber.dev>
2025-05-27 08:23:42 -0400

    Fix background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow bg-[#0f350f]">{children}</main>
         <Footer />
       </body>
     </html>
